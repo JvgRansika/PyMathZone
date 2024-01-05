51ee04dffdf9dffde0da2629a4b3388ddbe196e5
tharushaudana <tharushaudana519@gmail.com>
2024-01-05 17:03:43 +0530

    update

--- pymathzone/utils/matrix/determinant.py
+++ pymathzone/utils/matrix/determinant.py
@@ -2,10 +2,10 @@ def _det_of_2x2(m):
     return m.get(0, 0) * m.get(1, 1) - m.get(0, 1) * m.get(1, 0)
 
 def determinant(m):
-    if (m.rows == 1 and m.cols == 1):
+    if (m.rows == 1): # for 1x1
         return m.get(0, 0)
 
-    if (m.rows == 2): 
+    if (m.rows == 2): # for 2x2
         return _det_of_2x2(m)
 
     det = 0
