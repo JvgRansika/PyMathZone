d7af6c65897c5182b3061a7d77e75726ac7e8555
J.V.G. Ransika <jvgransika@gmail.com>
2024-01-05 11:07:25 +0530

    add __eq__() and relavent tests

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,9 +1,24 @@
 import unittest
+from pymathzone.matrix import Matrix
 
 
 class TestMatrix(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_add(self):
+        matrix1 = Matrix(2, 3, [[1, 2, 3], [4, 5, 6]])
+        matrix2 = Matrix(2, 3, [[7, 8, 9], [10, 11, 12]])
+        result = matrix1 + matrix2
+        self.assertEqual(result.data, [[8, 10, 12], [14, 16, 18]])
+
+    def test_equal(self):
+        matrix1 = Matrix(2, 3, [[1, 2, 3], [4, 5, 6]])
+        matrix2 = Matrix(2, 3, [[7, 8, 9], [10, 11, 12]])
+        matrix3 = Matrix(2, 3, [[7, 8, 9], [10, 11, 12]])
+        self.assertFalse(matrix1 == matrix2)
+        self.assertTrue(matrix2 == matrix3)
+
+        matrix1 = Matrix(1, 3, [1, 2, 3])
+        matrix2 = Matrix(2, 3, [[7, 8, 9], [10, 11, 12]])
+        self.assertFalse(matrix1==matrix2)
 
 
 if __name__ == '__main__':
